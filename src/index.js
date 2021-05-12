f9048f261f909c3cbe659e7fa097d83ed6d2d1af
Henk van der Spek <henk@vanmezelf.nl>
2021-05-12 10:03:34 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { createMachine, interpret } from "xstate";
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>XState Example</h1>
+<div>
+  Open the <strong>Console</strong> to view the machine output.
+</div>
+`;
+
+// Edit your machine(s) here
+const machine = createMachine({
+  id: "machine",
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" }
+    },
+    active: {
+      on: { TOGGLE: "inactive" }
+    }
+  }
+});
+
+// Edit your service(s) here
+const service = interpret(machine).onTransition((state) => {
+  console.log(state.value);
+});
+
+service.start();
+
+service.send("TOGGLE");
+service.send("TOGGLE");
